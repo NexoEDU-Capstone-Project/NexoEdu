59abfdeb92ef818548a12b1d842eea176364e565
Envy528 <carmonaballestasj528@gmail.com>
2026-07-12 20:16:28 -0500

    Estructuración MVC del proyecto

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import authRoutes from './routes/authRoutes.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Backend corriendo en puerto ${PORT}`));
