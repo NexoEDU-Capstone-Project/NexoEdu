a6932c9ac4c5405e69f11931a37b5f24b8f5f902
Ricardo031 <rjtb31@gmail.com>
2026-07-14 21:28:26 -0500

    api backend

--- backend/models/authModel.js
+++ backend/models/authModel.js
@@ -1,5 +1,10 @@
 import pool from '../db.js';
+import { createClient } from '@supabase/supabase-js'
 
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_SECRET_KEY  // La clave secreta
+)
 // Función para buscar un usuario por su username en la base de datos
 export async function buscarPorUsername(username) {
     // Realiza la consulta a la base de datos para obtener la información del usuario
